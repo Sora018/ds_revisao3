ba4c4d2db8853baaf421655cc8e1168cdd0b209a
emily moreira <141505569+emymoreira@users.noreply.github.com>
2024-02-23 20:48:03 -0300

    Update poli.js

--- poli.js
+++ poli.js
@@ -1,23 +1,26 @@
 class Pessoa {
-    constructor(nome) {
+    constructor(nome, idade) {
         this.nome = nome;
+        this.idade = idade;
     }
-}
-
-class Estudante extends Pessoa {
-    estudar() {
-        console.log(`${this.nome} está estudando.`);
+  }
+  
+  class Estudante extends Pessoa {
+    constructor(nome, idade, turma) {
+        super(nome, idade);
+        this.turma = turma;
     }
-}
-
-class Professor extends Pessoa {
-    ensinar() {
-        console.log(`${this.nome} está ensinando.`);
+  }
+  
+  class Professor extends Pessoa {
+    constructor(nome, idade, disciplina) {
+        super(nome, idade);
+        this.disciplina = disciplina;
     }
-}
-
-const estudante = new Estudante("Maria");
-const professor = new Professor("Robert");
-
-estudante.estudar();
-professor.ensinar();
+  }
+  
+  const estudante = new Estudante("Ana", 18, "3ºB");
+  const professor = new Professor("Rubens", 35, "História");
+  
+  console.log(`Nome: ${estudante.nome}, Idade: ${estudante.idade}, turma: ${estudante.turma}`);
+  console.log(`Nome: ${professor.nome}, idade: ${professor.idade}, disciplina: ${professor.disciplina}`);
