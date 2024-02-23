a1bf7fa0e2a235cae7d3001341bce2bdcb4f1e51
emily moreira <141505569+emymoreira@users.noreply.github.com>
2024-02-23 20:38:08 -0300

    Update poli.js

--- poli.js
+++ poli.js
@@ -1,12 +1,23 @@
-class Professor {
+class Pessoa {
     constructor(nome) {
         this.nome = nome;
     }
+}
 
+class Estudante extends Pessoa {
+    estudar() {
+        console.log(`${this.nome} está estudando.`);
+    }
+}
+
+class Professor extends Pessoa {
     ensinar() {
         console.log(`${this.nome} está ensinando.`);
     }
 }
+
+const estudante = new Estudante("Maria");
 const professor = new Professor("Robert");
 
+estudante.estudar();
 professor.ensinar();
