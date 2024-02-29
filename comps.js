3c72e43f08911fb7795a53f2311b97833b7c634b
Sora018 <renan.mc.2005.2014@gmail.com>
2024-02-28 22:01:17 -0300

    adicionamento do arquivo composição

---
+++ comps.js
@@ -0,0 +1,42 @@
+class Aluno {
+    constructor(nome) {
+      this.nome = nome;
+    }
+  }
+  
+  class Turma {
+    constructor(nome) {
+      this.nome = `Turma: ${nome}`;
+      this.alunos = [];
+    }
+  
+    adicionarAluno(aluno) {
+      this.alunos.push(aluno);
+    }
+  }
+  
+  class Escola {
+    constructor(nome) {
+      this.nome = `Escola: ${nome}`;
+      this.turmas = [];
+    }
+  
+    adicionarTurma(turma) {
+      this.turmas.push(turma);
+    }
+  }
+  
+  const aluno1 = new Aluno('João');
+  const aluno2 = new Aluno('Maria');
+  
+  const turma = new Turma('A');
+  turma.adicionarAluno(aluno1);
+  turma.adicionarAluno(aluno2);
+  
+  const minhaEscola = new Escola('ABC');
+  minhaEscola.adicionarTurma(turma);
+  
+  console.log(minhaEscola.nome);
+  minhaEscola.turmas.forEach(turma => {
+    console.log(turma.nome);
+  });  
