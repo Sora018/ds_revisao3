92481bdb0a6f21a0406f750ed4d8173890c739cb
Renan M.C <140671219+Sora018@users.noreply.github.com>
2024-02-28 22:22:17 -0300

    Update abstra.js

--- abstra.js
+++ abstra.js
@@ -1,13 +1,18 @@
 class Disciplina {
-    constructor(nome) {
+    constructor(nome, professor) {
         this.nome = nome;
+        this.professor = professor;
     }
 
     apresentar() {
-        console.log(`Esta é a disciplina de ${this.nome}.`);
+        console.log(`Esta é a disciplina de ${this.nome}, ministrada pelo professor ${this.professor}.`);
+    }
+
+    exibirProfessor() {
+        console.log(`O professor responsável pela disciplina de ${this.nome} é ${this.professor}.`);
     }
 }
 
-const disciplina = new Disciplina("Matemática");
-
+const disciplina = new Disciplina("Matemática", "Dr. Silva");
 disciplina.apresentar();
+disciplina.exibirProfessor();
